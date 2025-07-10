9c174ec3ec0540d2d7d904657a941b78e098ec38
mbarretodearau <m.b.a.f.barretodearaujo@tudelft.nl>
2025-07-10 13:21:15 +0200

    Deployed in tudelft-resources account

--- provider/deploy_tudelft_resources.sh
+++ provider/deploy_tudelft_resources.sh
@@ -4,15 +4,9 @@ timestamp=$(date +%Y-%m-%d-%H-%M)
 
 (cd lambdas ; zip -r ../tudelft-resources-$timestamp.zip .)
 
-aws s3 cp tudelft-resources-$timestamp.zip s3://tudelft-macro-resources/
+aws s3 cp tudelft-resources-$timestamp.zip s3://tudelft-macro-resources-provider-bucket/ --profile $1
 
 rm tudelft-resources-$timestamp.zip
 
-aws cloudformation deploy \
-  --template-file tudelft-resources.yaml \
-  --stack-name tudelft-resources \
-  --parameter-overrides \
-    Timestamp=$timestamp \
-  --capabilities CAPABILITY_NAMED_IAM
-
+aws cloudformation deploy --template-file tudelft-resources.yaml --stack-name tudelft-resources --parameter-overrides Timestamp="$timestamp" --capabilities CAPABILITY_NAMED_IAM --region eu-central-1 --profile "$1"
 
