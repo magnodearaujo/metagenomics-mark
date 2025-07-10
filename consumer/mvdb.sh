6bfac5845e5395f48933418b487ea10b0fb5fb6c
Magno de Araujo <35669207+magnodearaujo@users.noreply.github.com>
2025-07-10 14:11:08 +0200

    Update mvdb.sh
    Added --profile and --region

--- consumer/mvdb.sh
+++ consumer/mvdb.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-aws cloudformation deploy \
-  --template-file mvdb.yaml \
-  --stack-name mvdb-example \
-  --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file mvdb.yaml --stack-name mvdb-example --capabilities CAPABILITY_NAMED_IAM --region eu-central-1 --profile $1
